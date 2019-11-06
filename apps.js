cc4932831c04626eb178c4233ebcf97ea34eb7ca
Emmrakul <35402431+Emmrakul@users.noreply.github.com>
2019-11-06 14:25:24 -0500

    Get Started
    This addes and links together the basic html, css, and javascript files. It also adds a button that when clicked should display 10 random numbers between 1 and 100 in an unordered list. Functions to generate a random array and to print and array in list form.

---
+++ apps.js
@@ -0,0 +1,27 @@
+const myButton = document.querySelector('#button');
+let currentNumbers = [];
+
+
+// creates an array of (x) random numbers, between 0 and (limit)
+const getRandomNumbers = (x, limit) => {
+    let numberArray = [];
+    while(numberArray.length < x){
+        numberArray.push(Math.floor(Math.random() * limit));
+    }
+    return numberArray;
+};
+
+//prints the array (r) to an html list
+const printNumbers = (r) => {
+    let output = '<ul><li>';
+    output+= r.concat('</li><li>');
+    output+= '</li><ul>';
+    document.getElementById('output').innerHTML = output;
+    }
+}
+
+myButton.addEventListener('click', () => {
+    currentNumbers = getRandomNumbers(10, 100);
+    printNumbers(currentNumbers);
+    };
+);
